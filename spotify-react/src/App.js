62d4c5db69ae2a248aacad9506fda3b0c12e1672
CypherPhi <rodriguesvieira1911@gmail.com>
2025-01-31 14:19:47 -0300

    novo projeto react

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Imersao</h2>
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Sidebar />
+      <Main />
+      <Footer />
     </div>
   );
 }
