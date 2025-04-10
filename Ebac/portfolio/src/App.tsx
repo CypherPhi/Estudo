b40fbc23e8acf7d8e12eeec4e0faa5136686ba9d
CypherPhi <rodriguesvieira1911@gmail.com>
2025-04-10 11:44:54 -0300

    configurando novo ambiente

---
+++ Ebac/portfolio/src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyle from './styles'
+import Teste from './teste'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Teste />
+    </>
+  )
+}
+
+export default App
